68fbd3e518bb98e2a096d3ff6c179c6d52630be3
Tran Tan Phat <24120118@student.hcmus.edu.vn>
2025-06-13 14:14:04 +0700

    Change clustered method
    Change clustered test case generator function to a simpler method

--- Array_Generator.h
+++ Array_Generator.h
@@ -58,39 +58,17 @@ vector<int> sequentialTestUnit(int size)
 
 vector<int> clusteredTestUnit(int size)
 {   
-	random_device rd;
-	mt19937 g(rd());
-
-	vector<int> testUnit(size);
-
-	testUnit[0] = 0;
-
-	for (int i = 1; i <= size / 25; i++)
-	{
-		testUnit[i] = testUnit[i - 1] + 1 + rand() % 5;
-	}
-
-	int flag = testUnit[size/25];
-
-	shuffle(testUnit.begin(), testUnit.begin() + size / 25 + 1, g);
-
-	for (int i = 2; i <= 24; i++)
-	{
-		testUnit[((i-1)*size)/25 + 1] = flag + size + rand() % size;
-		for (int j = ((i - 1) * size) / 25 + 2; j <= (i * size) / 25; j++)
-		{
-			testUnit[j] = testUnit[j - 1] + 1 + rand() % 5;
-		}
-		flag= testUnit[(i*size) / 25];
-		shuffle(testUnit.begin()+ ((i - 1) * size) / 25 + 1, testUnit.begin() + (i  * size) / 25 + 1, g);
-	}
-
-	testUnit[(24*size)/25+1]=flag + rand() % size;
-
-	for(int i=(24*size)/25+2;i<size;i++)
-		testUnit[i] = testUnit[i - 1] + 1 + rand() % 5;
-	shuffle(testUnit.begin() + (24 * size) / 25 + 1, testUnit.end(), g);
-	return testUnit;
+	vector<int> testUnit;
+vector<int> pool(size * 5);
+iota(pool.begin(), pool.end(), 0);
+random_device rd;
+mt19937 g(rd());
+shuffle(pool.begin(), pool.end(), g);
+for (int i = 0; i < size; i++)
+{
+	testUnit.push_back(pool[i]);
+}
+return testUnit;
 }
 
 
